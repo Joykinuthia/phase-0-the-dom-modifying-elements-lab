618a959987901bdb6524b45a1125ea5393633ce9
Joykinuthia <joyrosekinuthia@gmail.com>
2025-03-20 11:41:56 +0300

    dom modifying eelments

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+// remove the main element
+document.querySelector("main#main").remove();
+
+//creating new header(h1) element
+let newHeader = document.createElement("h1");
+
+//assigning id
+newHeader.id = "victory";
+newHeader.innerHTML = "Joyrose is the champion";
+
+
+document.body.appendChild(newHeader);
+
+
